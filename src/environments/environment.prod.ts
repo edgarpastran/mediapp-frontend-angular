01500a36c176f969ca4b944c816dc4835315ce62
Edgar Pastran <edgarpastran@gmail.com>
2019-09-12 16:46:21 +0100

    Changing details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   HOST: 'http://134.209.167.81/mediapp-backend',
-  TOKEN_AUTH_USERNAME: 'mitomediapp',
-  TOKEN_AUTH_PASSWORD: 'mito89codex',
+  TOKEN_AUTH_USERNAME: 'mediapp-client-id',
+  TOKEN_AUTH_PASSWORD: 'mediapp-codex',
   TOKEN_NAME: 'access_token'
 };
